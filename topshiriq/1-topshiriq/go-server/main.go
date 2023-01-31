7af5ed00c4f7d754ecc74c922f27934b1a6f3378
NamozovAzizbek <salom_007@proton.me>
2023-01-31 11:09:05 +0500

    git

---
+++ topshiriq/1-topshiriq/go-server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil{
+		log.Fatal(err)
+	}
+
+}
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello")
+}
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil{					// post, put, patch requestlari hamda requestning tanasini o'qish uchun
+		fmt.Fprintf(w, "Parseform() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful \n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s \n", name)
+	fmt.Fprintf(w, "Address = %s", address)
+
+
+}
